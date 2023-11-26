7a0cefb3e87ba1536e4edbdff89b30bfd4858067
olivier <olivier.winter@hotmail.fr>
2023-11-26 11:53:13 +0000

    version 1.5.0 revert minFR parameter

--- pykilosort/ibl.py
+++ pykilosort/ibl.py
@@ -121,8 +121,8 @@ def ibl_pykilosort_params(bin_file):
     params.preprocessing_function = 'destriping'
     params.channel_detection_method = 'raw_correlations'
     params.probe = probe_geometry(bin_file)
-    params.minFR = 0
-    params.minfr_goodchannels = 0
+    # params.minFR = 0
+    # params.minfr_goodchannels = 0
     # params = {k: dict(params)[k] for k in sorted(dict(params))}
     return dict(params)
 
