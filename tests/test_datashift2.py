0c9d163e6327eb40c6862cb1afee6bac98ca8b54
olivier <olivier.winter@hotmail.fr>
2024-07-01 19:25:25 +0100

    type check in datashift2

--- tests/test_datashift2.py
+++ tests/test_datashift2.py
@@ -7,7 +7,7 @@ def test_shift_data():
     data = np.asfortranarray(np.random.randint(-100, 100, size=(65000, 384), dtype=np.int16))
     transformation = np.random.normal(size=(384, 384))
     test_result = shift_data(data, transformation)
-    assert type(test_result == np.ndarray)
+    assert isinstance(test_result, np.ndarray)
     assert test_result.dtype == np.int16
     assert np.isfortran(test_result)
     assert np.allclose(test_result, np.asfortranarray((data @ transformation.T).astype("int16")))
