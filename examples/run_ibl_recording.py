33d9a1f12bd048d76178fc8c3f33daa0ff517088
owinter <olivier.winter@hotmail.fr>
2025-01-04 18:14:55 +0100

    remove bulky datasets after run

--- examples/run_ibl_recording.py
+++ examples/run_ibl_recording.py
@@ -1,32 +1,31 @@
 """
 Usage:
->>> run_ibl_recording.py eid probe_name
->>> run_ibl_recording.py aec2b14f-5dbc-400b-bf2e-dd13e711e2ff probe00 --cache_dir /mnt/s0/ONE  --scratch_dir /mnt/ssd1/scratch/iblsorter
+>>> run_ibl_recording.py pid
+>>> run_ibl_recording.py aec2b14f-5dbc-400b-bf2e-dd13e711e2ff --cache_dir /mnt/s0/ONE  --scratch_dir /mnt/ssd1/scratch/iblsorter
 
 Usage with docker::
-
 >>> sudo docker compose up -d
->>> docker compose exec spikesorter python /root/Documents/PYTHON/pykilosort/docker/run_ibl_recording.py aec2b14f-5dbc-400b-bf2e-dd13e711e2ff probe00 --cache_dir /mnt/s0/ONE
+>>> docker compose exec spikesorter python /root/Documents/PYTHON/pykilosort/docker/run_ibl_recording.py aec2b14f-5dbc-400b-bf2e-dd13e711e2ff --cache_dir /mnt/s0/ONE
 
 It is safer to stop / start the docker container for each run to flush the GPU memory on some machines
 """
-
-from ibllib.pipes.ephys_tasks import SpikeSorting, EphysPulses
-
 from pathlib import Path
-from one.api import ONE
+import shutil
 
 from pydantic import Field, DirectoryPath, FilePath
 from pydantic_settings import BaseSettings, CliPositionalArg
 
+from one.api import ONE
+from ibllib.pipes.ephys_tasks import SpikeSorting, EphysPulses
 
+
+REMOVE_DATA = True
 SCRATCH_DIR = Path.home().joinpath('scratch', 'iblsorter')
 override_params = {}  # here it is possible to set some parameters for the run
 
 
 class CommandLineArguments(BaseSettings, cli_parse_args=True):
-    eid: CliPositionalArg[str] = Field(description='experiment ID')
-    pname: CliPositionalArg[str] = Field(description='probe name')
+    pid: CliPositionalArg[str] = Field(description='probe insertion ID')
     cache_dir: DirectoryPath | None = Field(description='The full path to the ONE cache directory', default=None)
     scratch_dir: DirectoryPath | None = Field(description='The full path to the SSD scratch', default=SCRATCH_DIR)
 
@@ -34,11 +33,19 @@ class CommandLineArguments(BaseSettings, cli_parse_args=True):
 if __name__ == "__main__":
     args = CommandLineArguments()
     one = ONE(base_url='https://alyx.internationalbrainlab.org', cache_dir=args.cache_dir)
-    session_path = one.eid2path(args.eid)
-    sync_job = EphysPulses(session_path, one=one, pname=args.pname, sync_collection='raw_ephys_data/probe00', location="EC2", on_error='raise')
+    eid, pname = one.pid2eid(args.pid)
+    session_path = one.eid2path(eid)
+    sync_job = EphysPulses(session_path, one=one, pname=pname, sync_collection='raw_ephys_data/probe00', location="EC2", on_error='raise')
     sync_job.run()
-    ssjob = SpikeSorting(session_path, one=one, pname=args.pname, device_collection='raw_ephys_data', location="EC2", on_error='raise',
+    ssjob = SpikeSorting(session_path, one=one, pname=pname, device_collection='raw_ephys_data', location="EC2", on_error='raise',
                          scratch_folder=args.scratch_dir)
     ssjob.run()
     ssjob.register_datasets(labs='churchlandlab', force=True)
     sync_job.register_datasets(labs='churchlandlab', force=True)
+    if REMOVE_DATA:
+        dir_sorter = session_path.joinpath('spike_sorters', 'iblsorter', pname)
+        print(f'Removing sorter directory: {dir_sorter}')
+        shutil.rmtree(dir_sorter)
+        for f in session_path.joinpath('raw_ephys_data', pname).glob('*.ap.*bin'):
+            print(f'Removing raw data: {f}')
+            f.unlink()
